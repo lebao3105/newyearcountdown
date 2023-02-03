062292cd96a430fbc6adf61b5995a8ff9a22983d
lebao3105 <bao12345yocoo@gmail.com>
2023-02-03 18:31:25 +0700

    Time remaining

--- newyearcounter.cpp
+++ newyearcounter.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <math.h>
+#include <stdbool.h>
 #include "newyearcounter.hpp"
 
 using namespace std;
+  
+bool checkLeapYear(int year) 
+{ 
+    if (year % 400 == 0) 
+        return true; 
+  
+    if (year % 4 == 0 && year % 100 != 0) 
+        return true;
+ 
+    return false; 
+} 
 
 int dayinYear(date_now dt) {
     int s; // result
@@ -14,7 +27,10 @@ int dayinYear(date_now dt) {
                 s += 30;
                 break;
             case 2:
-                s += 28 + (dt.year % 4 == 0 && dt.year % 100) || (dt.year % 400 == 0);
+                if (checkLeapYear(dt.year) == true)
+                    s += 29;
+                else
+                    s += 28;
                 break;
             default:
                 s += 31;
@@ -23,6 +39,10 @@ int dayinYear(date_now dt) {
     return s;
 }
 
+float get_year_progress(int today, int daysofyear) {
+    float onepercent = daysofyear / 100;
+    return (float) today / onepercent;
+}
 
 int main() {
     newyear_time j;
@@ -30,15 +50,32 @@ int main() {
     time_now tm2;
 
     int s = dayinYear(dt);
-    int daysremaining = 365 - s;
-    int monthsremaining = (daysremaining%365)/30;
-    float year_progress = s / 3.65;
+    int daysofyear = 365;
 
-    // cout << s;
-    // cout << daysremaining;
+    if (checkLeapYear(dt.year) == true)
+        daysofyear = 366;
+
+    int daysremaining = daysofyear - s;
+    int monthsremaining = (daysremaining%daysofyear)/30;
+    float year_progress = roundf(get_year_progress(s, daysofyear) * 100) / 100;
+    
+    if (j.sc > 0)
+        j.mn --;
+
+    if (j.mn > 0)
+        j.hr --;
+    
+    if (j.sc && j.mn > 0)
+        daysremaining --;
+
+    // Our current location
     cout << "The current local time is: " << asctime(ltm);
     cout << "This year " << dt.year << " is " << year_progress << "%" << " complete." << endl;
+
+    // Date / Time remaining
     cout << "Next year " << dt.year + 1 << " starts after totally " << \
-        daysremaining << " day(s)." << endl;
+        daysremaining << " day(s), and about " << \
+        j.hr << " hour(s) " << j.mn << " minute(s) " << j.sc << " second(s)." << endl;
+
     return 0;
 }
